0673eb7ac5f6105914d9fa199261116e9b205f14
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-09-09 09:58:00 +0300

    my commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+        name: name,
+        email: email
+    })
+    })
+    .then(res => res.json())
+    .then(data => {
+        const myid = data["id"]
+        const body = document.querySelector("body")
+        body.innerHTML = myid
+    })
+    .catch(error => {
+        alert("Unauthorized Access");
+        document.body.innerHTML = error.message
+    })
+}
